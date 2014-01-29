e4b50f44bd8c2c0fa783e0b7f0a5693700d55d99
U-STB1012213\b1012213 <b1012213@stb1012213.(none)>
2014-01-30 00:39:27 +0900

    add

---
+++ FixPoint.java
@@ -0,0 +1,7 @@
+public class FixPoint {
+	public static void main(String [] args) {
+		char c = 34;
+		System.out.println(s + c + s + c + ';' + '}');
+	}
+	static String s = "public class FixPoint {\n\t public static void main(String [] args) {\n\t\t char c = 34; System.out.println(s + c + s + c + ';' + '}'); } static String s= ";
+}
