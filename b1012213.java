9337812bc81e737e911e8a6ecec9c7f98a770428
Yukineko <yukineko.mew7@gmail.com>
2014-02-04 20:42:48 +0900

    significiant modification

---
+++ b1012213.java
@@ -0,0 +1,48 @@
+package b1012213;
+//@author b1012213 Yuki Ogasawara
+
+public class b1012213 {
+    public static void main(String [] args) {
+        char space = 32;
+        char wQuote = 34;
+        char comma = 44;
+        char semiColon = 59;
+        char brace = 125;
+        for(String line : src) {
+            System.out.println( line );
+        }
+        for(String line : src) {
+            for( int j=0; j<8; j++ ) { System.out.print(space); }
+            System.out.println(wQuote + line + wQuote + comma);
+        }
+        for( int i=0; i<8;i++ ) { System.out.print(space); }
+        System.out.print(brace);
+        System.out.println(semiColon);
+        System.out.println(brace);
+    }
+    static String src [] = {
+        "package b1012213;",
+        "//@author b1012213 Yuki Ogasawara",
+        "",
+        "public class b1012213 {",
+        "    public static void main(String [] args) {",
+        "        char space = 32;",
+        "        char wQuote = 34;",
+        "        char comma = 44;",
+        "        char semiColon = 59;",
+        "        char brace = 125;",
+        "        for(String line : src) {",
+        "            System.out.println( line );",
+        "        }",
+        "        for(String line : src) {",
+        "            for( int j=0; j<8; j++ ) { System.out.print(space); }",
+        "            System.out.println(wQuote + line + wQuote + comma);",
+        "        }",
+        "        for( int i=0; i<8;i++ ) { System.out.print(space); }",
+        "        System.out.print(brace);",
+        "        System.out.println(semiColon);",
+        "        System.out.println(brace);",
+        "    }",
+        "    static String src [] = {",
+        };
+}
